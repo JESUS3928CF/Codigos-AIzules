9265de1ab53215b85e1c4fbb035e0f248b4b0f39
Jesús Cf <jesus3928cf@gmail.com>
2024-04-18 14:59:58 -0500

    feat(detector): Implemented face detection
    Added code to detect and recognize faces in images using image processing techniques.

--- mslearn-ai-vision/Labfiles/04-face/Python/computer-vision/detect-people.py
+++ mslearn-ai-vision/Labfiles/04-face/Python/computer-vision/detect-people.py
@@ -1,48 +1,96 @@
-from dotenv import load_dotenv
-import os
-from array import array
-from PIL import Image, ImageDraw
-import sys
-import time
-from matplotlib import pyplot as plt
-import numpy as np
+from dotenv import load_dotenv  # Importa la función load_dotenv desde la biblioteca dotenv
+import os  # Importa el módulo os para interactuar con el sistema operativo
+from array import array  # Importa el tipo de datos array desde la biblioteca array
+from PIL import Image, ImageDraw  # Importa las clases Image y ImageDraw del módulo PIL (Python Imaging Library)
+import sys  # Importa el módulo sys para interactuar con el intérprete de Python
+import time  # Importa el módulo time para manejar el tiempo
+from matplotlib import pyplot as plt  # Importa el módulo pyplot de la biblioteca matplotlib para trazar gráficos
+import numpy as np  # Importa el módulo numpy para realizar cálculos numéricos
 
-# Import namespaces
+# Importa el espacio de nombres necesario para interactuar con Azure AI Vision
+import azure.ai.vision as sdk  
 
-
-
+# Define la función principal del programa
 def main():
-    global cv_client
+    global cv_client  # Declara una variable global cv_client
 
     try:
-        # Get Configuration Settings
+        # Carga la configuración desde un archivo .env
         load_dotenv()
-        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
-        ai_key = os.getenv('AI_SERVICE_KEY')
+        ai_endpoint = os.getenv('AI_SERVICE_ENDPOINT')  # Obtiene el punto de conexión del servicio de AI desde las variables de entorno
+        ai_key = os.getenv('AI_SERVICE_KEY')  # Obtiene la clave del servicio de AI desde las variables de entorno
 
-        # Get image
-        image_file = 'images/people.jpg'
-        if len(sys.argv) > 1:
+        # Obtiene la ruta de la imagen a analizar
+        image_file = 'images/people.jpg'  # Define una ruta de imagen predeterminada
+        if len(sys.argv) > 1:  # Si se proporciona una ruta de imagen como argumento en la línea de comandos, la usa en lugar de la predeterminada
             image_file = sys.argv[1]
 
-        # Authenticate Azure AI Vision client
-
+        # Autentica el cliente de Azure AI Vision
+        cv_client = sdk.VisionServiceOptions(ai_endpoint, ai_key)
         
-        # Analyze image
+        # Analiza la imagen
         AnalyzeImage(image_file, cv_client)
 
-    except Exception as ex:
+    except Exception as ex:  # Captura cualquier excepción que ocurra y la imprime
         print(ex)
 
 
+# Define la función para analizar la imagen
 def AnalyzeImage(image_file, cv_client):
     print('\nAnalyzing', image_file)
 
-    # Specify features to be retrieved (PEOPLE)
+    # Especifica las características a recuperar (en este caso, PEOPLE)
+    analysis_options = sdk.ImageAnalysisOptions()
+        
+    features = analysis_options.features = (
+        sdk.ImageAnalysisFeature.PEOPLE  # Establece la característica a analizar como PEOPLE (personas)
+    )    
 
 
-    # Get image analysis
+    # Obtiene el análisis de la imagen
+    image = sdk.VisionSource(image_file)  # Crea un objeto de origen de imagen desde el archivo de imagen
+    image_analyzer = sdk.ImageAnalyzer(cv_client, image, analysis_options)  # Crea un analizador de imagen
+    result = image_analyzer.analyze()  # Realiza el análisis de la imagen
+    
+    if result.reason == sdk.ImageAnalysisResultReason.ANALYZED:  # Si la imagen se analizó correctamente
+        # Obtiene las personas en la imagen
+        if result.people is not None:
+            print("\nPeople in image:")  # Imprime un encabezado
+            
+            # Prepara la imagen para dibujar
+            image = Image.open(image_file)  # Abre la imagen usando PIL
+            fig = plt.figure(figsize=(image.width/100, image.height/100))  # Crea una figura de matplotlib
+            plt.axis('off')  # Desactiva los ejes en la trama
+            draw = ImageDraw.Draw(image)  # Crea un objeto de dibujo en la imagen
+            color = 'cyan'  # Define el color para dibujar los cuadros delimitadores
+            
+            # Itera sobre cada persona detectada
+            for detected_people in result.people:
+                # Dibuja el cuadro delimitador de la persona si la confianza es mayor al 50%
+                if detected_people.confidence > 0.5:
+                    # Dibuja el cuadro delimitador de la persona
+                    r = detected_people.bounding_box
+                    bounding_box = ((r.x, r.y), (r.x + r.w, r.y + r.h))  # Define las coordenadas del cuadro delimitador
+                    draw.rectangle(bounding_box, outline=color, width=3)  # Dibuja el cuadro delimitador en la imagen
+                    
+                    # Imprime la confianza de la persona detectada
+                    print(" {} (confidence: {:.2f}%)".format(detected_people.bounding_box, detected_people.confidence * 100))
+                        
+            # Guarda la imagen con las personas detectadas
+            plt.imshow(image)  # Muestra la imagen con las personas detectadas
+            plt.tight_layout(pad=0)  # Ajusta el diseño de la trama
+            outputfile = 'detected_people.jpg'  # Nombre del archivo de salida
+            fig.savefig(outputfile)  # Guarda la imagen con las personas detectadas
+            print('  Results saved in', outputfile)  # Imprime un mensaje indicando dónde se guardaron los resultados
+        
+    else:  # Si el análisis falla
+        error_details = sdk.ImageAnalysisErrorDetails.from_result(result)  # Obtiene los detalles del error
+        print(" Analysis failed.")  # Imprime un mensaje de error
+        print("   Error reason: {}".format(error_details.reason))  # Imprime la razón del error
+        print("   Error code: {}".format(error_details.error_code))  # Imprime el código de error
+        print("   Error message: {}".format(error_details.message))  # Imprime el mensaje de error
 
 
+# Llama a la función main si este script se ejecuta directamente
 if __name__ == "__main__":
     main()
--- mslearn-ai-vision/Labfiles/04-face/Python/face-api/analyze-faces.py
+++ mslearn-ai-vision/Labfiles/04-face/Python/face-api/analyze-faces.py
@@ -1,41 +1,100 @@
+# Importar la función load_dotenv para cargar variables de entorno desde un archivo .env
 from dotenv import load_dotenv
+# Importar el módulo os para acceder a variables de entorno y operar con archivos
 import os
+# Importar las clases Image y ImageDraw de la biblioteca PIL para trabajar con imágenes
 from PIL import Image, ImageDraw
+# Importar la función pyplot de matplotlib para visualizar imágenes
 from matplotlib import pyplot as plt
 
-# Import namespaces
+# Importar los espacios de nombres necesarios de Azure Cognitive Services para el reconocimiento facial
+from azure.cognitiveservices.vision.face import FaceClient
+from azure.cognitiveservices.vision.face.models import FaceAttributeType
+from msrest.authentication import CognitiveServicesCredentials
 
-
+# Definir la función principal del programa
 def main():
-
-    global face_client
+    global face_client  # Declarar face_client como una variable global
 
     try:
-        # Get Configuration Settings
+        # Obtener la configuración desde el archivo .env
         load_dotenv()
-        cog_endpoint = os.getenv('AI_SERVICE_ENDPOINT')
-        cog_key = os.getenv('AI_SERVICE_KEY')
+        cog_endpoint = os.getenv('AI_SERVICE_ENDPOINT')  # Obtener el punto final del servicio desde las variables de entorno
+        cog_key = os.getenv('AI_SERVICE_KEY')  # Obtener la clave de la API desde las variables de entorno
 
-        # Authenticate Face client
+        # Autenticar el cliente Face utilizando las credenciales
+        credentials = CognitiveServicesCredentials(cog_key)
+        face_client = FaceClient(cog_endpoint, credentials)
 
-
-        # Menu for face functions
+        # Mostrar un menú para seleccionar una función
         print('1: Detect faces\nAny other key to quit')
-        command = input('Enter a number:')
-        if command == '1':
-            DetectFaces(os.path.join('images','people.jpg'))
+        command = input('Enter a number:')  # Solicitar al usuario que ingrese un número
+        if command == '1':  # Si el usuario ingresa '1', llamar a la función DetectFaces
+            DetectFaces(os.path.join('images','l3xoor.jpg'))  # Pasar la ruta de la imagen como argumento
 
-    except Exception as ex:
-        print(ex)
+    except Exception as ex:  # Capturar y manejar cualquier excepción
+        print(ex)  # Imprimir el mensaje de error si ocurre alguna excepción
 
+# Definir la función para detectar caras en una imagen
 def DetectFaces(image_file):
-    print('Detecting faces in', image_file)
+    print('Detecting faces in', image_file)  # Imprimir un mensaje indicando que se están detectando caras en la imagen
 
-    # Specify facial features to be retrieved
+    # Especificar las características faciales que se van a recuperar durante la detección
+    features = [FaceAttributeType.occlusion,
+                FaceAttributeType.blur,
+                FaceAttributeType.glasses]
 
+    # Obtener las caras en la imagen utilizando el cliente Face
+    with open(image_file, mode="rb") as image_data:
+        detected_faces = face_client.face.detect_with_stream(image=image_data,
+                                                                return_face_attributes=features,
+                                                                return_face_id=False)
 
-    # Get faces
+        if len(detected_faces) > 0:  # Si se detectan una o más caras en la imagen
+            print(len(detected_faces), 'faces detected.')  # Imprimir el número de caras detectadas
 
+            # Preparar la imagen para dibujar los cuadros alrededor de las caras
+            fig = plt.figure(figsize=(8, 6))  # Crear una figura con un tamaño específico
+            plt.axis('off')  # Desactivar los ejes de la figura
+            image = Image.open(image_file)  # Abrir la imagen utilizando PIL
+            draw = ImageDraw.Draw(image)  # Crear un objeto ImageDraw para dibujar sobre la imagen
+            color = 'lightgreen'  # Definir el color para los cuadros de las caras
+            face_count = 0  # Inicializar un contador para el número de caras
 
+            # Dibujar y anotar cada cara detectada en la imagen
+            for face in detected_faces:
+                # Obtener las propiedades de la cara detectada
+                face_count += 1  # Incrementar el contador de caras
+                print('\nFace number {}'.format(face_count))  # Imprimir el número de cara
+
+                detected_attributes = face.face_attributes.as_dict()  # Convertir los atributos de la cara a un diccionario
+                if 'blur' in detected_attributes:  # Si se detecta borrosidad en la cara
+                    print(' - Blur:')  # Imprimir un mensaje indicando la detección de borrosidad
+                    for blur_name in detected_attributes['blur']:  # Iterar sobre los diferentes tipos de borrosidad
+                        print('   - {}: {}'.format(blur_name, detected_attributes['blur'][blur_name]))  # Imprimir los detalles de la borrosidad
+
+                if 'occlusion' in detected_attributes:  # Si se detecta ocultamiento en la cara
+                    print(' - Occlusion:')  # Imprimir un mensaje indicando la detección de ocultamiento
+                    for occlusion_name in detected_attributes['occlusion']:  # Iterar sobre los diferentes tipos de ocultamiento
+                        print('   - {}: {}'.format(occlusion_name, detected_attributes['occlusion'][occlusion_name]))  # Imprimir los detalles del ocultamiento
+
+                if 'glasses' in detected_attributes:  # Si se detectan gafas en la cara
+                    print(' - Glasses:{}'.format(detected_attributes['glasses']))  # Imprimir un mensaje indicando la detección de gafas
+
+                # Dibujar un cuadro alrededor de la cara detectada en la imagen
+                r = face.face_rectangle  # Obtener el rectángulo delimitador de la cara
+                bounding_box = ((r.left, r.top), (r.left + r.width, r.top + r.height))  # Definir las coordenadas del cuadro delimitador
+                draw.rectangle(bounding_box, outline=color, width=5)  # Dibujar el cuadro delimitador alrededor de la cara
+                annotation = 'Face number {}'.format(face_count)  # Crear una etiqueta para anotar la cara
+                plt.annotate(annotation,(r.left, r.top), backgroundcolor=color)  # Anotar la cara en la imagen con la etiqueta
+
+            # Guardar la imagen con los cuadros alrededor de las caras detectadas
+            plt.imshow(image)  # Mostrar la imagen con los cuadros de las caras
+            outputfile = 'detected_faces.jpg'  # Definir el nombre del archivo de salida
+            fig.savefig(outputfile)  # Guardar la figura como una imagen
+
+            print('\nResults saved in', outputfile)  # Imprimir un mensaje indicando que los resultados se han guardado en el archivo
+
+# Comprobar si este script es el script principal
 if __name__ == "__main__":
-    main()
+    main()  # Llamar a la función principal si este script es el script principal
